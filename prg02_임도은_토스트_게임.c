f2c16d7e13754e7c148d983cabfbf704b3b76493
122a <91895434+122a@users.noreply.github.com>
2021-11-24 22:20:45 +0900

    Update prg02_임도은_토스트_게임.c

--- prg02_임도은_토스트_게임.c
+++ prg02_임도은_토스트_게임.c
@@ -5,20 +5,25 @@
 
 #define WIDTH   18
 #define HEIGHT  10
+#define _B 1975.533
+#define _F 1396.913
 
 char board_1[HEIGHT][WIDTH];
 char board_2[HEIGHT][WIDTH];
 
-// 토스트_내용물 설정
 int arr[5] = {'@', '*', '^', '%', '='};
 int input;
-int a = 0;
+int second = 60;
+int a, check, count, min = 0;
+int price, price_e = 0;
 
 void initialize(int, int);
 void gotoxy(int x, int y);
 void play();
+void print_toast();
 
-void gotoxy(int x, int y) { // x,y 좌표를 사용
+// x,y 좌표를 사용
+void gotoxy(int x, int y) {
     COORD Cur;
     Cur.X = x;
     Cur.Y = y;
@@ -26,6 +31,7 @@ void gotoxy(int x, int y) { // x,y 좌표를 사용
 }
 
 void initialize(int start_x, int start_y) {
+    /*첫 번째 보드*/
     // 위, 아래 가로벽 
     for (int i = 0; i < WIDTH; i++) {
         board_1[0][i] = board_1[HEIGHT - 1][i] = '#';
@@ -42,8 +48,7 @@ void initialize(int start_x, int start_y) {
         board_1[i][0] = board_1[i][WIDTH - 1] = '#';
     }
 
-    /////////////////////////////////////////////////
-    
+    /*두 번째 보드*/
     // 위, 아래 가로벽 
     for (int i = 0; i < WIDTH; i++) {
         board_2[0][i] = board_2[HEIGHT - 1][i] = '#';
@@ -64,7 +69,7 @@ void initialize(int start_x, int start_y) {
 // 화면 출력 
 void display() {
     system("cls");
-    //예시 화면
+    //보드_1 화면
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             printf("%c", board_1[i][j]);
@@ -72,7 +77,7 @@ void display() {
         printf("\n");
     }
 
-    //참여 화면
+    //보드_2 화면
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             printf("%c", board_2[i][j]);
@@ -87,17 +92,32 @@ void display() {
     gotoxy(24, 4);
     printf("-------------------------------------\n");
 
-    gotoxy(24, 5);
-    printf("손님:\t샌드위치 하나 주세요.\n");
+    //주문 시 대사
+    if (check == 0) {
+        gotoxy(24, 5);
+        printf("손님:\t샌드위치 하나 주세요.\n");
 
-    gotoxy(24, 6);
-    printf("알바:\t네, 알겠습니다!\n");
+        gotoxy(24, 6);
+        printf("알바:\t네, 알겠습니다!\n");
+    }
+
+    //틀린 주문 시 대사
+    else if (check == 1) {
+        gotoxy(24, 5);
+        printf("손님:\t제가 시킨 거랑 다르잖아요!");
+
+        gotoxy(24, 6);
+        printf("알바:\t으악! 죄송합니다!");
+    }
 
     gotoxy(24, 8);
-    printf("시간: \t000\n");
+    printf("시간: \t%d분 %02d\n", min, second);
 
     gotoxy(24, 9);
-    printf("판매 수:000개\n");
+    printf("판매 금:%05d원\n", price);
+
+    //gotoxy(24, 9);
+    //printf("판매 수:%03d개\n", count);
 
     gotoxy(24, 10);
     printf("-------------------------------------\n");
@@ -112,32 +132,66 @@ void display() {
     printf("무엇을 올릴까?\n");
 
     //재료 입력
-    while (1)
-    {
-        gotoxy(24, 17);
-        printf("ANSWER: ");
-        scanf_s("%d", &input);
+    if (check == 0) {
+        while (1) {
+            gotoxy(24, 17);
+            printf("ANSWER: ");
+            scanf_s(" %d", &input);
 
-        //1 ~ 5입력 시
-        if (6 > input && input > 0){
-            play();
-            break;
+            //1 ~ 5입력 시
+            if (6 > input && input > 0) {
+                play();
+                break;
+            }
+            //샌드위치 제출
+            else if (input == 0) {
+                play();
+            }
+            //아예 번호가 틀리면 재개
+            else {
+                printf("\a");
+                continue;
+            }
         }
-
-        //아예 번호가 틀리면 재개
-        else{
-            printf("\a");
-            continue;
-        }
-
-        //햄버거 제출 생각해야 함.
     }
+
+    check = 0;
     gotoxy(24, 23);
+
 }
 
+void sell() {
+    gotoxy(24, 5);
+    printf("손님:\t와! 맛있겠다! 감사합니다.");
+
+    gotoxy(24, 6);
+    printf("알바:\t샌드위치 나왔습니다!");
+
+    price += price_e;
+    Beep(_F, 100);
+    Sleep(200);
+    Beep(_B, 100);
+    Sleep(1800);
+    count++;
+    initialize(1, 1);
+    print_toast();
+    price_e, a = 0;
+    display();
+}
+
+//주문 샌드위치 설정
 void print_toast() {
-    //주문 샌드위치 설정
+    //샌드위치 높이 설정
     int SIZE = rand() % 7;
+
+    switch (SIZE) {
+    case 0:
+        SIZE += 3;
+
+    case 1:
+        SIZE += 2;
+    }
+
     for (int i = 0; i < SIZE; i++) {
         char inner = arr[rand() % 5];
         for (int j = 0; j < 10; j++) {
@@ -164,37 +218,72 @@ void print_toast() {
 void play() {
     while (1) {
         if (board_1[8 - a][4] == ' ') {
-            break;
+            sell();
         }
         //번호가 맞을 경우
         else if (arr[input - 1] == board_1[8 - a][4]) {
             for (int j = 0; j < 10; j++) {
                 board_2[8 - a][4 + j] = arr[input - 1];
             }
+
+            if (arr[input - 1] == '@')
+                price_e += 800;
+            else
+                price_e += 1000;
+
             a++;
             display();
             //다음 단계로 넘어가야 함.
         }
         //번호가 틀렸을 경우
-        else
-            for (int j = 0; j < 10; j++) {
-                board_2[8 - a][4 + j] = arr[input - 1];
+        else {
+            if (input == 0) {
+                for (int j = 0; j < 10; j++) {
+                    board_2[8 - a][4 + j] = ' ';
+                }
             }
-        printf("\a");
-        display();
-        //초기화 해야함.
-        break;
+            else {
+                for (int j = 0; j < 10; j++) {
+                    board_2[8 - a][4 + j] = arr[input - 1];
+                }
+            }
+            price_e = 0;
+            printf("\a");
+            check = 1;
+            display();
+            Sleep(1800);
+            initialize(1, 1);
+            print_toast();
+            a = 0;
+            display();
+        }
     }
 }
 
+void time() {
+    while (1) {
+        Sleep(1000);
+        second--;
+        system("cls");
+        if (second < 0) {
+            min++;
+            second = 59;
+        }
+        gotoxy(24, 8);
+        printf("시간: \t%d분 %02d초\n", min, second);
+    }
+}
 
 int main(void) {
     printf("샌드위치 가게에 오신 걸 환영합니다!\n\n\n");
 
+    printf("엔터 키 입력 시 시작합니다!");
+    getchar();
+
     initialize(1, 1);
     print_toast();
     display();
-
+    time();
     printf("\n");
     return 0;
 }
